a24f917f0a586f24de7d58efc271e23c94245340
Ho Je <83208807+zini9188@users.noreply.github.com>
2022-11-22 04:37:29 +0900

    [Silver III] Title: 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+#define MAX 1000000000
+long long int X, Y, Z;
+int cnt;
+
+int main() {
+    cin >> X >> Y;
+    Z = (Y * 100) / X;
+
+    if (Z >= 99) {
+        cout << "-1";
+        return 0;
+    }
+    int left = 0, right = MAX;
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        int temp = (Y + mid) * 100 / (X + mid);
+        if (Z < temp)
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+    cout << left;
+}
