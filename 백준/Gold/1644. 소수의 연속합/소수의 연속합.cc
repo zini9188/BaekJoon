6a4f8f9db3f77a66597468ba79c1fde815213c47
Ho Je <83208807+zini9188@users.noreply.github.com>
2022-11-22 22:49:40 +0900

    Update 소수의 연속합.cc

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -4,24 +4,33 @@
 
 using namespace std;
 
-int main() {
-    int N;
-    vector<int> arr(4000001);
-    cin >> N;
+vector<int> eratos(int N) { // 에라토스테네스의 체
+    vector<int> prime(N + 1); // N개의 숫자가 들어갈 수 있는 벡터 생성
 
-    for (int i = 2; i <= N; i++)
-        arr[i] = i;
+    for (int i = 2; i <= N; i++) // 벡터에 2~의 숫자를 넣어줌
+        prime[i] = i;
 
     for (int i = 2; i * i <= N; i++) {
-        if (arr[i]) {
-            for (int j = i * i; j <= N; j += i) {
-                arr[j] = 0;
+        if (prime[i]) { // 아직 지워지지 않은 수라면
+            for (int j = i * i; j <= N; j += i) { // 해당 값에 n배수를 전부 제거해준다.
+                prime[j] = 0;
             }
         }
     }
+
+    return prime;
+}
+
+int main() {
+    int N;
+    cin >> N;
+
     vector<int> connect;
-    for (int i = 2; i <= N; i++) {
-        if (arr[i]) {
+    vector<int> prime(N + 1);
+    prime = eratos(N);
+
+    for (int i = 2; i <= N; i++) { // N 이하 소수들 전부 벡터에 넣어준다
+        if (prime[i]) {
             connect.push_back(i);
         }
     }
@@ -29,7 +38,7 @@ int main() {
     int left = 0, right = 0;
     int sum = 2;
     int answer = 0;
-    while (right < connect.size() && left <= right) {
+    while (right < connect.size() && left <= right) { // 투 포인터 사용
         if (sum == N) {
             answer++;
             sum += connect[++right];
