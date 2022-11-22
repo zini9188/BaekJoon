116654eea6f41689649b3d97938877bb07bd02b7
Ho Je <83208807+zini9188@users.noreply.github.com>
2022-11-23 01:03:19 +0900

    Update 구간 합 구하기 4.cc

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -5,14 +5,22 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
+    /*
+     *  입출력 속도를 향상시키는 함수들
+     *  flush를 덜하기에 입출력이 향상
+     */
+    ios::sync_with_stdio(false); 
     cin.tie(nullptr);
+    
     int N, M;
     cin >> N >> M;
 
     vector<int> arr(N + 1);
     vector<int> sum(N + 1);
 
+    /*
+     * 미리 모든 합을 구해놓고 구간 입력이 들어오면 두 차이를 구해준다.
+     */
     for (int index = 1; index <= N; index++) {
         cin >> arr[index];
         sum[index] = arr[index] + sum[index - 1];
