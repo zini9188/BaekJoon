f8030fbb819a7dbcb34fd8f9e40eda7dc8fd7661
Ho Je <83208807+zini9188@users.noreply.github.com>
2022-11-29 04:58:39 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 12 ms, Memory: 2548 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -16,27 +16,26 @@ int main() {
     int N, K, answer = 987654321;
     cin >> N >> K;
     vector<bool> visited(100001);
-    priority_queue<pair<int, int>, vector<pair<int,int>>, greater<>> pq;
-    pq.push(make_pair(0, N));
+    priority_queue<pair<int, int>> pq;
+    pq.push(make_pair(0, -N));
     visited[N] = true;
     while (!pq.empty()) {
-        int second = pq.top().first;
-        int position = pq.top().second;
+        int second = -pq.top().first;
+        int position = -pq.top().second;
         pq.pop();
-
         if (position == K) {
             answer = min(second, answer);
         }
         if (position * 2 < MAX_SIZE && !visited[position * 2]) {
-            pq.push(make_pair(second, position * 2));
+            pq.push(make_pair(-second, -(position * 2)));
             visited[position * 2] = true;
         }
         if (position + 1 < MAX_SIZE && !visited[position + 1]) {
-            pq.push(make_pair(second + 1, position + 1));
+            pq.push(make_pair(-(second + 1), -(position + 1)));
             visited[position + 1] = true;
         }
         if (position - 1 >= 0 && !visited[position - 1]) {
-            pq.push(make_pair(second + 1, position - 1));
+            pq.push(make_pair(-(second + 1), -(position - 1)));
             visited[position - 1] = true;
         }
     }
