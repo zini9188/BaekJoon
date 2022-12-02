71a710b1ff9757fc5a03d2089dacd84d1e8702ef
Ho Je <83208807+zini9188@users.noreply.github.com>
2022-12-02 17:01:42 +0900

    [Gold V] Title: 암호 만들기, Time: 0 ms, Memory: 2168 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int L, C;
+vector<string> words(16);
+vector<bool> visited(16);
+string aeiou = "aeiou";
+vector<string> answers;
+
+bool checkPossible(const string &word) {
+    int vowel = 0, consonant = 0;
+    for (auto w: word) {
+        if (find(aeiou.begin(), aeiou.end(), w) != aeiou.end()) {
+            vowel++;
+        } else {
+            consonant++;
+        }
+    }
+
+    if (vowel >= 1 && consonant >= 2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void dfs(int length, const string &word) {
+    if (length == L) {
+        if (checkPossible(word)) {
+            answers.push_back(word);
+        }
+        return;
+    }
+
+    for (int i = 0; i < C; i++) {
+        if (!visited[i] && word.back() < words[i][0]) {
+            visited[i] = true;
+            dfs(word.length() + 1, word + words[i]);
+            visited[i] = false;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> L >> C;
+    for (int i = 0; i < C; i++) {
+        cin >> words[i];
+    }
+
+    for (int i = 0; i < C; i++) {
+        visited[i] = true;
+        dfs(1, words[i]);
+        visited[i] = false;
+    }
+
+    sort(answers.begin(), answers.end());
+
+    for(const auto& answer : answers){
+        cout << answer << "\n";
+    }
+}
