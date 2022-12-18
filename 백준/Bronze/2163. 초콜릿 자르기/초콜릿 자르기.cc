07a6e20fcfc9c440c1cfd6ca46f2470fa2bf7f84
Ho Je <83208807+zini9188@users.noreply.github.com>
2022-12-18 14:22:41 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N, M;
+    cin >> N >> M;
+    cout << N * M - 1;
+}
