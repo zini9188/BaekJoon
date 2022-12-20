4ae05d258d6315428e50eaf624fef6539e5ae4fc
Ho Je <83208807+zini9188@users.noreply.github.com>
2022-12-20 16:06:33 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.cc
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -8,25 +8,19 @@ int main() {
     cin.tie(nullptr);
     cout.tie(nullptr);
     int n, k;
-    cin >> n >> k;
-
     vector<int> dp(10001);
     vector<int> coin(101);
-
-    for (int i = 0; i < n; i++) {
-        cin >> coin[i];
-    }
+    
+    cin >> n >> k;    
+    for (int i = 0; i < n; i++) cin >> coin[i];    
     dp[0] = 1;
-
     for (int i = 1; i <= k; i++) {
         if (i % coin[0] == 0) dp[i] = dp[i - coin[0]];
     }
 
     for (int i = 1; i < n; i++) {
-        int cnt = 0;
-        for (int j = 1; j <= k; j++) {
-            if (j >= coin[i]) cnt = dp[j - coin[i]];
-            dp[j] += cnt;
+        for (int j = coin[i]; j <= k; j++) {
+            dp[j] += dp[j - coin[i]];
         }
     }
     cout << dp[k];
