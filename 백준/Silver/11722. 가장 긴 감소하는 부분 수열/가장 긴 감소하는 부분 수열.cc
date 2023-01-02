04c7511caaa9fb47fda1fef48931a068e4fd53a8
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-01-03 00:10:59 +0900

    99퍼에서 틀리던 이유

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -4,7 +4,10 @@
 #include <algorithm>
 
 using namespace std;
-
+// 해당 문제는 99퍼에서 틀렸는데
+// ans를 0으로 초기화시킨게 문제였다/
+// 입력이 하나 들어오는 경우 for문이 돌지 않아
+// ans가 최대값으로 초기화가 되지 않아 값이 0이 나온다.
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
