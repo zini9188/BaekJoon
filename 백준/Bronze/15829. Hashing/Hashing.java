af2046eb6a730576bb1f916004ca38e09013ba8d
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-01-09 20:37:02 +0900

    [Bronze II] Title: Hashing, Time: 136 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,18 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int L = Integer.parseInt(scanner.nextLine());
-        String str = scanner.nextLine();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int L = Integer.parseInt(br.readLine());
+        String str = br.readLine();
         int R = 31;
         int M = 1234567891;
         long res = 0;
         long r = 1;
-        for (char c : str.toCharArray()) {
-            res += ((c - 'a' + 1) * r) % M;
+        for (int i = 0; i < L; i++) {
+            res += ((str.charAt(i) - 'a' + 1) * r) % M;
             r = (r * R) % M;
         }
-        System.out.println(res);
+        bw.write(String.valueOf(res));
+        bw.flush();
+        br.close();
+        bw.close();
     }
 }
