399f09f8a8bf3ed5e388ce6ab73485623f837454
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-01-18 18:20:41 +0900

    [Silver IV] Title: 카드2, Time: 200 ms, Memory: 45688 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+
+        bw.write(String.valueOf(queue.peek()));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
+
+
+
