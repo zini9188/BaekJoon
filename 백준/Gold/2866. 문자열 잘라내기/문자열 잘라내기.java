3b278ad9e2be400110cf3334c7e2651a4b3a5a32
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-02-04 23:11:13 +0900

    [Gold V] Title: 문자열 잘라내기, Time: 364 ms, Memory: 32248 KB -BaekjoonHub

--- 백준/Gold/2866. 문자열 잘라내기/문자열 잘라내기.java
+++ 백준/Gold/2866. 문자열 잘라내기/문자열 잘라내기.java
@@ -2,9 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
+    static int R, C;
     static char[][] table;
-    static String[] rotate;
+    static ArrayList<String> rotate;
     static HashSet<String> dict;
     static int count = 0;
 
@@ -12,21 +12,19 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer tokenizer = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(tokenizer.nextToken());
-        M = Integer.parseInt(tokenizer.nextToken());
-        table = new char[M][N];
-        rotate = new String[M];
-        for (int i = 0; i < N; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < M; j++) {
-                table[j][i] = line.charAt(j);
-            }
+        R = Integer.parseInt(tokenizer.nextToken());
+        C = Integer.parseInt(tokenizer.nextToken());
+        table = new char[R][C];
+        rotate = new ArrayList<>();
+        for (int i = 0; i < R; i++) {
+            table[i] = br.readLine().toCharArray();
         }
-        for (int i = 0; i < M; i++) {
-            String temp = Arrays.toString(table[i])
-                    .replaceAll("[^a-z]", "")
-                    .replaceAll(", ", "");
-            rotate[i] = temp;
+        for (int i = 0; i < C; i++) {
+            StringBuilder builder = new StringBuilder();
+            for (int j = 0; j < R; j++) {
+                builder.append(table[j][i]);
+            }
+            rotate.add(builder.toString());
         }
         solution();
         bw.write(count + "");
@@ -40,7 +38,7 @@ public class Main {
     }
 
     private static void binarySearch() {
-        int left = 1, right = N - 1;
+        int left = 1, right = R - 1;
         while (left <= right) {
             int mid = (left + right) / 2;
             if (findDuple(mid)) {
@@ -54,9 +52,13 @@ public class Main {
 
     private static boolean findDuple(int start) {
         dict = new HashSet<>();
-        for (int j = 0; j < M; j++) {
-            dict.add(rotate[j].substring(start));
+        for (int i = 0; i < C; i++) {
+            String temp = rotate.get(i).substring(start);
+            if (dict.contains(temp)) {
+                return false;
+            }
+            dict.add(temp);
         }
-        return dict.size() == M;
+        return true;
     }
 }
