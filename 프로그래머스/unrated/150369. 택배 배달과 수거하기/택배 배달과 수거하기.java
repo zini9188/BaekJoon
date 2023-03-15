29dd98a57653735924981bffe27f612cdccadca5
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-03-15 10:18:37 +0900

    [unrated] Title: 택배 배달과 수거하기, Time: 1204.31 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -5,21 +5,21 @@ class Solution {
             while (n > 0 && deliveries[n - 1] == 0 && pickups[n - 1] == 0) {
                 n--;
             }
-            doSome(deliveries, n, cap);
-            doSome(pickups, n, cap);
+            moveTruck(deliveries, n, cap);
+            moveTruck(pickups, n, cap);
             second += (n * 2L);            
         }
         return second;
     }
 
-    private void doSome(int[] boxes, int n, int cap) {
-        long truck = 0;
+    private void moveTruck(int[] boxes, int n, int cap) {
+        int truck = 0;
         for (int i = n - 1; i >= 0; i--) {
             if (truck + boxes[i] <= cap) {
                 truck += boxes[i];
                 boxes[i] = 0;
             } else {
-                long canCarry = cap - truck;
+                int canCarry = cap - truck;
                 truck += canCarry;
                 boxes[i] -= canCarry;
             }
