016583ee98ae1608c521890ca0f0640714959518
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-03-15 10:25:20 +0900

    [unrated] Title: 이모티콘 할인행사, Time: 0.72 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,27 +1,25 @@
 import java.util.ArrayList;
 
 class Solution {
-    int[] discount = {10, 20, 30, 40};
     ArrayList<ArrayList<Integer>> discountCases = new ArrayList<>();
     ArrayList<int[]> answers = new ArrayList<>();
 
     public int[] solution(int[][] users, int[] emoticons) {
-        int len = emoticons.length;
-        dfs(0, len, new ArrayList<>());
-        int[] answer = {0, 0};
+        findDiscountCases(0, emoticons.length, new ArrayList<>());
         for (ArrayList<Integer> discountCase : discountCases) {
             int service = 0;
             int total = 0;
             for (int[] user : users) {
-                int userRate = user[0];
-                int userLimit = user[1];
+                int wantDiscount = user[0];
+                int limitPrice = user[1];
                 int sum = 0;
                 for (int i = 0; i < discountCase.size(); i++) {
-                    if (discountCase.get(i) >= userRate) {
-                        sum += emoticons[i] * (100 - discountCase.get(i)) / 100;
+                    int discount = discountCase.get(i);
+                    if (discount >= wantDiscount) {
+                        sum += emoticons[i] * (100 - discount) / 100;
                     }
                 }
-                if (sum >= userLimit) {
+                if (sum >= limitPrice) {
                     service++;
                 } else {
                     total += sum;
@@ -33,15 +31,15 @@ class Solution {
         return answers.get(0);
     }
 
-    private void dfs(int index, int count, ArrayList<Integer> temp) {
-        if (index == count) { // 설정된 할인 비율로 값 찾기
+    private void findDiscountCases(int index, int count, ArrayList<Integer> temp) {
+        if (index == count) {
             discountCases.add((ArrayList<Integer>) temp.clone());
             return;
         }
 
-        for (int i = 0; i < 4; i++) {
-            temp.add(discount[i]);
-            dfs(index + 1, count, temp);
+        for (int i = 1; i <= 4; i++) {
+            temp.add(i * 10);
+            findDiscountCases(index + 1, count, temp);
             temp.remove(temp.size() - 1);
         }
     }
