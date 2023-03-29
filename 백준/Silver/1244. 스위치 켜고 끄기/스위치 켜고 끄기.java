4fa00be375e0d455fb247b9d19fa390917182beb
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-03-29 11:39:50 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -2,55 +2,64 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[] switches;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
+
+        int switchCount = Integer.parseInt(br.readLine());
         StringTokenizer tokenizer = new StringTokenizer(br.readLine());
-        int[] switches = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
+        switches = new int[switchCount + 1];
+        for (int i = 1; i <= switchCount; i++) {
             switches[i] = Integer.parseInt(tokenizer.nextToken());
         }
+
         int studentCount = Integer.parseInt(br.readLine());
         for (int i = 0; i < studentCount; i++) {
             tokenizer = new StringTokenizer(br.readLine());
             int gender = Integer.parseInt(tokenizer.nextToken());
-            int switchCount = Integer.parseInt(tokenizer.nextToken());
+            int target = Integer.parseInt(tokenizer.nextToken());
             if (gender == 1) {
-                maleDo(switchCount, switches);
+                toggleByMale(target, switches);
             } else {
-                femaleDo(switchCount, switches);
+                toggleByFemale(target, switches);
             }
         }
-        StringBuilder builder = new StringBuilder();
-        for (int i = 1; i <= N; i++) {
-            builder.append(switches[i]).append(" ");
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 1; i <= switchCount; i++) {
+            result.append(switches[i]).append(" ");
             if (i % 20 == 0) {
-                builder.append("\n");
+                result.append("\n");
             }
         }
-        bw.write(builder.toString());
+        bw.write(result.toString());
         bw.flush();
         bw.close();
         br.close();
     }
 
-    private static void femaleDo(int count, int[] switches) {
-        int left = count - 1, right = count + 1;
-        changeSwitchState(switches, count);
-        while (left > 0 && right < switches.length && switches[left] == switches[right]) {
-            changeSwitchState(switches, left--);
-            changeSwitchState(switches, right++);
+    private static void toggleByFemale(int target, int[] switches) {
+        int left = target - 1, right = target + 1;
+        toggleSwitch(switches, target);
+        while (isInRange(switches.length, left, right) && switches[left] == switches[right]) {
+            toggleSwitch(switches, left--);
+            toggleSwitch(switches, right++);
         }
     }
 
-    private static void maleDo(int count, int[] switches) {
-        for (int i = count; i < switches.length; i += count) {
-            changeSwitchState(switches, i);
+    private static void toggleByMale(int target, int[] switches) {
+        for (int i = target; i < switches.length; i += target) {
+            toggleSwitch(switches, i);
         }
     }
 
-    private static void changeSwitchState(int[] switches, int index) {
-        switches[index] = switches[index] == 1 ? 0 : 1;
+    private static void toggleSwitch(int[] switches, int index) {
+        switches[index] ^= 1;
+    }
+
+    private static boolean isInRange(int range, int left, int right) {
+        return left >= 1 && right < range;
     }
 }
