af8216af3bf5d3b99c09aa305de2d911adf0cca1
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-04-16 18:56:48 +0900

    [Silver IV] Title: 국영수, Time: 656 ms, Memory: 57580 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.java
+++ 백준/Silver/10825. 국영수/국영수.java
@@ -1,35 +1,34 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-class Exam implements Comparable<Exam> {
-    String name;
-    int kor, eng, math;
-
-    public Exam(String name, int kor, int eng, int math) {
-        this.name = name;
-        this.kor = kor;
-        this.eng = eng;
-        this.math = math;
-    }
-
-    @Override
-    public int compareTo(Exam other) {
-        if (this.kor == other.kor) {
-            if (this.eng == other.eng) {
-                if (this.math == other.math) {
-                    return name.compareTo(other.name);
-                }
-                return other.math - this.math;
-            }
-            return this.eng - other.eng;
-        }
-        return other.kor - this.kor;
-    }
-}
-
 public class Main {
+    static class Exam implements Comparable<Exam> {
+        String name;
+        int kor, eng, math;
+
+        public Exam(String name, int kor, int eng, int math) {
+            this.name = name;
+            this.kor = kor;
+            this.eng = eng;
+            this.math = math;
+        }
+
+        @Override
+        public int compareTo(Exam other) {
+            if (this.kor == other.kor) {
+                if (this.eng == other.eng) {
+                    if (this.math == other.math) {
+                        return name.compareTo(other.name);
+                    }
+                    return other.math - this.math;
+                }
+                return this.eng - other.eng;
+            }
+            return other.kor - this.kor;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -40,7 +39,7 @@ public class Main {
             exams.add(new Exam(tokenizer.nextToken(), Integer.parseInt(tokenizer.nextToken()), Integer.parseInt(tokenizer.nextToken()), Integer.parseInt(tokenizer.nextToken())));
         }
         StringBuilder builder = new StringBuilder();
-        while (!exams.isEmpty()){
+        while (!exams.isEmpty()) {
             builder.append(exams.poll().name).append("\n");
         }
         bw.write(builder.toString());
