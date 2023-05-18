e3d6139a4dfc7a4998a1cb04d012fd933e7e2b37
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-05-18 16:42:59 +0900

    [unrated] Title: 덧칠하기, Time: 0.79 ms, Memory: 66.5 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -1,18 +1,13 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
+        int maxPainted = 0;
         int answer = 0;
-        boolean[] wall = new boolean[n + 1];
-        for(int i = 0; i < section.length; i++){
-            wall[section[i]] = true;
-        }
         
         for(int i = 0; i < section.length; i++){
             int target = section[i];
-            if(wall[target]){ 
+            if(maxPainted <= target){
+                maxPainted = target + m;
                 answer++;
-                for(int j = target; j < target + m && j <= n; j++){
-                    wall[j] = false;
-                }
             }
         }
         
