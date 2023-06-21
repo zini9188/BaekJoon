67830672c504e3884ba6a87082f95593962bac7a
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-06-21 14:07:44 +0900

    [Silver III] Title: 1로 만들기, Time: 220 ms, Memory: 18304 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
+            }
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
+            }
+        }
+
+        bw.write(dp[N] + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
