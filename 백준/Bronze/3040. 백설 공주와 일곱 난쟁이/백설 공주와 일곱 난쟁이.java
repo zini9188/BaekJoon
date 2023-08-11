56a0dd6aaa13df81e0d1d47f594816b15c1ba1cf
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-08-11 19:59:31 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 120 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -3,7 +3,6 @@ import java.io.*;
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
         int[] dwarf = new int[9];
@@ -14,23 +13,18 @@ public class Main {
             score += n;
         }
 
-        StringBuilder builder = new StringBuilder();
         for (int i = 0; i < 8; i++) {
             for (int j = i + 1; j < 9; j++) {
                 if (dwarf[i] + dwarf[j] == score - 100) {
                     for(int k = 0; k < 9; k++){
                         if(k != i && k != j){
-                            builder.append(dwarf[k]).append("\n");
+                            System.out.println(dwarf[k]);
                         }
                     }
-
-                    bw.write(builder.toString());
-                    bw.flush();
-                    bw.close();
-                    br.close();
                     break;
                 }
             }
         }
+        br.close();
     }
 }
