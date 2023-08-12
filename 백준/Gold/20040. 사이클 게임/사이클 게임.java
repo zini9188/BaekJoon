298df6c4eb84e927dbce9965cad43be1beb06870
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-08-13 00:16:56 +0900

    [Gold IV] Title: 사이클 게임, Time: 728 ms, Memory: 125092 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int[] parents;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(tokenizer.nextToken());
+        int M = Integer.parseInt(tokenizer.nextToken());
+
+        parents = new int[N];
+        for (int i = 0; i < N; i++) {
+            parents[i] = i;
+        }
+
+        int answer = 0;
+        for (int i = 1; i <= M; i++) {
+            tokenizer = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(tokenizer.nextToken());
+            int b = Integer.parseInt(tokenizer.nextToken());
+            if (find(a) == find(b)) {
+                answer = i;
+                break;
+            }
+            union(a, b);
+        }
+
+        bw.write(answer + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static void union(int a, int b) {
+        int x1 = find(a);
+        int x2 = find(b);
+        if (x1 != x2) {
+            parents[x2] = x1;
+        }
+    }
+
+    static int find(int x) {
+        if (parents[x] == x) {
+            return x;
+        }
+        return parents[x] = find(parents[x]);
+    }
+}
