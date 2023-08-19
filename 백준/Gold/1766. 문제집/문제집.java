7a559480a36d638dcf277e8ebff349812e823132
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-08-20 06:41:30 +0900

    [Gold II] Title: 문제집, Time: 584 ms, Memory: 47700 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer tokenizer;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        tokenizer = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(tokenizer.nextToken());
+        int M = Integer.parseInt(tokenizer.nextToken());
+
+        int[] possible = new int[N + 1];
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < N + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            tokenizer = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(tokenizer.nextToken());
+            int B = Integer.parseInt(tokenizer.nextToken());
+            possible[B]++;
+            graph.get(A).add(B);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o));
+        for (int i = 1; i < N + 1; i++) {
+            if (possible[i] == 0) {
+                pq.add(i);
+            }
+        }
+
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(0);
+        while (!pq.isEmpty()) {
+            int current = pq.poll();
+
+            answer.add(current);
+            for (Integer next : graph.get(current)) {
+                if (--possible[next] == 0) {
+                    pq.add(next);
+                }
+            }
+        }
+
+        for (int i = 1; i < N + 1; i++) {
+            sb.append(answer.get(i)).append(" ");
+        }
+
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+}
