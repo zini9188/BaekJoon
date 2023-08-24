3e27c8728dd4446874b7ef7bc3f3d14b7128258a
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-08-24 10:05:21 +0900

    [Bronze II] Title: 공 바꾸기, Time: 128 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer tokenizer;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        tokenizer = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(tokenizer.nextToken());
+        int M = Integer.parseInt(tokenizer.nextToken());
+        int[] basket = new int[N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            basket[i] = i;
+        }
+
+        for (int m = 0; m < M; m++) {
+            tokenizer = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(tokenizer.nextToken());
+            int j = Integer.parseInt(tokenizer.nextToken());
+            swap(i, j, basket);
+        }
+
+        for (int i = 1; i < N + 1; i++) {
+            sb.append(basket[i]).append(" ");
+        }
+
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    static void swap(int x, int y, int[] arr) {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+}
