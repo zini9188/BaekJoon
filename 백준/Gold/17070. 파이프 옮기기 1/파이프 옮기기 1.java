1b7c59cc2e534cc1cd6458d077cdf18ba61269d2
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-08-30 15:34:40 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 76 ms, Memory: 11732 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,117 +1,42 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static final int HORIZONTAL = 0, VERTICAL = 1, DIAGONAL = 2, WALL = 1;
-    static int[][] house;
-    static int[] dx = {0, 1, 1};
-    static int[] dy = {1, 0, 1};
-    static int N;
-    static int answer = 0;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        N = Integer.parseInt(br.readLine());
-        house = new int[N + 1][N + 1];
-        for (int x = 1; x <= N; x++) {
-            StringTokenizer tokenizer = new StringTokenizer(br.readLine());
-            for (int y = 1; y <= N; y++) {
-                house[x][y] = Integer.parseInt(tokenizer.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[][] A = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        Pipe pipe = new Pipe(HORIZONTAL, new Point(1, 1), new Point(1, 2));
-        findPath(pipe);
-
-        bw.write(answer + "");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-
-    public static void findPath(Pipe pipe) {
-        if (pipe.isDestination()) {
-            answer++;
-            return;
+        int[][][] dp = new int[3][N][N]; // 0: 가로, 1: 세로, 2: 대각선
+        for (int i = 1; i < N; i++) {
+            if (A[0][i] == 1) break;
+            dp[0][0][i] = 1;
         }
 
-        for (int dir = 0; dir < 3; dir++) {
-            if (pipe.checkDirection(dir)) {
-                Point nextEndPoint = pipe.end.nextPoint(dir);
-                if (dir == DIAGONAL) {
-                    if (pipe.end.checkDiagonal()) {
-                        Pipe nextPipe = pipe.move(nextEndPoint, dir);
-                        findPath(nextPipe);
-                    }
-                } else {
-                    if (nextEndPoint.isInRange() && nextEndPoint.isNotWall()) {
-                        Pipe nextPipe = pipe.move(nextEndPoint, dir);
-                        findPath(nextPipe);
-                    }
+        for (int i = 1; i < N; i++) {
+            for (int j = 1; j < N; j++) {
+                // 대각선
+                if (A[i][j] == 0 && A[i - 1][j] == 0 && A[i][j - 1] == 0) {
+                    dp[2][i][j] = dp[0][i - 1][j - 1] + dp[1][i - 1][j - 1] + dp[2][i - 1][j - 1];
+                }
+
+                if (A[i][j] == 0) {
+                    // 가로
+                    dp[0][i][j] = dp[0][i][j - 1] + dp[2][i][j - 1];
+                    // 세로
+                    dp[1][i][j] = dp[1][i - 1][j] + dp[2][i - 1][j];
                 }
             }
         }
-    }
-
-    static class Pipe {
-        int direction;
-        Point front, end;
-
-        public Pipe(int direction, Point front, Point end) {
-            this.direction = direction;
-            this.front = front;
-            this.end = end;
-        }
-
-        public Pipe move(Point point, int dir) {
-            return new Pipe(dir, end, point);
-        }
-
-        public boolean checkDirection(int dir) {
-            if (direction == HORIZONTAL) {
-                return dir != VERTICAL;
-            }
-            if (direction == VERTICAL) {
-                return dir != HORIZONTAL;
-            }
-            return true;
-        }
-
-        public boolean isDestination() {
-            return end.x == N && end.y == N;
-        }
-    }
-
-    static class Point {
-        int x, y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public Point nextPoint(int dir) {
-            return new Point(x + dx[dir], y + dy[dir]);
-        }
-
-        public boolean isInRange() {
-            return x <= N && y <= N;
-        }
-
-        public boolean isNotWall() {
-            return house[x][y] != WALL;
-        }
-
-        public boolean checkDiagonal() {
-            return x + 1 <= N && y + 1 <= N
-                    && nextPoint(HORIZONTAL).isNotWall()
-                    && nextPoint(VERTICAL).isNotWall()
-                    && nextPoint(DIAGONAL).isNotWall();
-        }
+        System.out.println(dp[0][N - 1][N - 1] + dp[1][N - 1][N - 1] + dp[2][N - 1][N - 1]);
     }
 }
