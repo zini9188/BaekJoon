f46ca6409c5582f6edba53e71140e6002d2934d6
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-08-30 16:12:39 +0900

    [Silver II] Title: 외판원 순회 2, Time: 300 ms, Memory: 14632 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,55 +1,56 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int N;
-    private static int[][] A;
-    private static boolean[] visited;
-    private static int answer;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer tokenizer;
+    static int[][] cost;
+    static int N;
+    static boolean[] visited;
+    static int answer = Integer.MAX_VALUE;
 
-        A = new int[N][N];
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        visited = new boolean[N];
+        cost = new int[N][N];
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            tokenizer = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                A[i][j] = Integer.parseInt(st.nextToken());
+                cost[i][j] = Integer.parseInt(tokenizer.nextToken());
             }
         }
 
-        answer = Integer.MAX_VALUE;
         for (int i = 0; i < N; i++) {
-            visited = new boolean[N];
             visited[i] = true;
-            dfs(i, i, 0);
+            dfs(i, i, 0, 0);
+            visited[i] = false;
         }
-        System.out.println(answer);
+
+        sb.append(answer);
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
     }
 
-    private static void dfs(int now, int start, int total) {
-        if (isAllVisited()) {
-            if (A[now][start] == 0) return;
-            answer = Math.min(answer, total + A[now][start]);
+    static void dfs(int start, int from, int depth, int totalCost) {
+        if (depth == N - 1 && cost[from][start] != 0) {
+            answer = Math.min(answer, totalCost + cost[from][start]);
             return;
         }
 
-        for (int i = 0; i < N; i++) {
-            if (!visited[i] && A[now][i] != 0) {
-                visited[i] = true;
-                dfs(i, start, total + A[now][i]);
-                visited[i] = false;
+        for (int to = 0; to < N; to++) {
+            if (visited[to] || cost[from][to] == 0) {
+                continue;
             }
-        }
-    }
 
-    private static boolean isAllVisited() {
-        for (int i = 0; i < N; i++) {
-            if (!visited[i]) return false;
+            visited[to] = true;
+            dfs(start, to, depth + 1, totalCost + cost[from][to]);
+            visited[to] = false;
         }
-        return true;
     }
 }
