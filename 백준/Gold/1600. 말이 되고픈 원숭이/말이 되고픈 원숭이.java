da0e8f1200e07708dfd32ec9c31bb15028a4ef96
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-08-30 17:54:10 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 512 ms, Memory: 55416 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,103 +1,93 @@
-import jdk.nashorn.api.scripting.ScriptObjectMirror;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static final int[] dx = {1, 0, -1, 0};
+    private static final int[] dy = {0, 1, 0, -1};
+    private static final int[] hx = {1, 2, -1, 2, -2, 1, -2, -1};
+    private static final int[] hy = {2, 1, 2, -1, 1, -2, -1, -2};
+    private static int K, W, H;
+    private static int[][] A;
+    private static int answer;
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer tokenizer;
-    static StringBuilder sb = new StringBuilder();
-    static int W, H;
-    static int[][] board;
-    static int[][] dist;
-    static boolean[][][] visited;
-    static int answer = Integer.MAX_VALUE;
-    static int[] hdx = {-1, 1, 2, 2, 1, -1, -2, -2};
-    static int[] hdy = {-2, -2, -1, 1, 2, 2, 1, -1};
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        K = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        W = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
 
-    public static void main(String[] args) throws IOException {
-        int K = Integer.parseInt(br.readLine());
-        tokenizer = new StringTokenizer(br.readLine());
-        W = Integer.parseInt(tokenizer.nextToken());
-        H = Integer.parseInt(tokenizer.nextToken());
-
-        visited = new boolean[H][W][K + 1];
-        board = new int[H][W];
+        A = new int[H][W];
         for (int i = 0; i < H; i++) {
-            tokenizer = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < W; j++) {
-                board[i][j] = Integer.parseInt(tokenizer.nextToken());
+                A[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        dist = new int[H][W];
-        for (int i = 0; i < H; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
+        answer = bfs();
+        System.out.println(answer);
+    }
 
-        Queue<Monkey> queue = new ArrayDeque<>();
-        queue.add(new Monkey(0, 0, K, 0));
-        dist[0][0] = 0;
-        visited[0][0][K] = true;
+    private static int bfs() {
+        Queue<Node> q = new ArrayDeque<>();
+        int[][][] visited = new int[H][W][40];
+        q.add(new Node(0, 0, 0));
+        visited[0][0][0] = 1;
 
-        while (!queue.isEmpty()) {
-            Monkey monkey = queue.poll();
-            int x = monkey.x;
-            int y = monkey.y;
-            int k = monkey.k;
-            int c = monkey.c;
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
 
-            if (x == H - 1 && y == W - 1) {
-                answer = Math.min(answer, c);
-                continue;
+            if (cur.x == H - 1 && cur.y == W - 1) {
+                return visited[cur.x][cur.y][cur.k] - 1;
             }
 
+            for (int i = 0; i < 8; i++) {
+                int nx = cur.x + hx[i];
+                int ny = cur.y + hy[i];
+                int nk = cur.k + 1;
+
+                if (!inRange(nx, ny)) {
+                    continue;
+                }
+                if (visited[nx][ny][nk] != 0 || A[nx][ny] == 1 || nk > K)
+                    continue;
+
+                visited[nx][ny][nk] = visited[cur.x][cur.y][cur.k] + 1;
+                q.add(new Node(nx, ny, nk));
+            }
             for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (isInRange(nx, ny) && !visited[nx][ny][k] && board[nx][ny] == 0) {
-                    visited[nx][ny][k] = true;
-                    queue.add(new Monkey(nx, ny, k, c + 1));
-                }
-            }
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
 
-            if (k > 0) {
-                for (int i = 0; i < 8; i++) {
-                    int nx = x + hdx[i];
-                    int ny = y + hdy[i];
-                    if (isInRange(nx, ny) && !visited[nx][ny][k - 1] && board[nx][ny] == 0) {
-                        visited[nx][ny][k - 1] = true;
-                        queue.add(new Monkey(nx, ny, k - 1, c + 1));
-                    }
+                if (!inRange(nx, ny)) {
+                    continue;
                 }
+                if (visited[nx][ny][cur.k] != 0 || A[nx][ny] == 1) {
+                    continue;
+                }
+                visited[nx][ny][cur.k] = visited[cur.x][cur.y][cur.k] + 1;
+                q.add(new Node(nx, ny, cur.k));
             }
         }
-
-        sb.append(answer == Integer.MAX_VALUE ? -1 : answer);
-        bw.write(sb.toString());
-        bw.close();
-        br.close();
+        return -1;
     }
 
-    private static boolean isInRange(int x, int y) {
-        return x >= 0 && y >= 0 && x < H && y < W;
+    private static boolean inRange(int x, int y) {
+        return x >= 0 && x < H && y >= 0 && y < W;
     }
 
-    static class Monkey {
-        int x, y, k, c;
+    private static class Node {
+        int x, y, k;
 
-        public Monkey(int x, int y, int k, int c) {
+        public Node(int x, int y, int k) {
             this.x = x;
             this.y = y;
             this.k = k;
-            this.c = c;
         }
     }
 }
