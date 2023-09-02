deea3633d4a07c7ff2bc2b6958571545a26495d0
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-09-03 02:26:44 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 1388 ms, Memory: 372436 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -1,24 +1,20 @@
 import java.io.*;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.StringTokenizer;
 
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer tokenizer;
     static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        tokenizer = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(tokenizer.nextToken());
-        int L = Integer.parseInt(tokenizer.nextToken());
+    public static void main(String[] args) throws IOException {Reader r = new Reader();
+        int N = r.nextInt();
+        int L = r.nextInt();
 
         Deque<Node> deque = new ArrayDeque<>();
-        tokenizer = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            int a = Integer.parseInt(tokenizer.nextToken());
+            int a =  r.nextInt();
             while (!deque.isEmpty() && deque.getLast().val > a) {
                 deque.removeLast();
             }
@@ -41,6 +37,52 @@ public class Main {
         public Node(int idx, int val) {
             this.idx = idx;
             this.val = val;
+        }
+    }
+
+    private static class Reader {
+        private final int BUFFER_SIZE = 1 << 16;
+        private final DataInputStream din;
+        private final byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            din.close();
         }
     }
 }
