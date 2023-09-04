ef7025eca16d40904dcfb56ba86a2e994c1dd33c
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-09-04 16:21:34 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1880 ms, Memory: 339784 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer tokenizer;
+    static StringBuilder sb = new StringBuilder();
+    static int N, M, K;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+
+    public static void main(String[] args) throws IOException {
+        tokenizer = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(tokenizer.nextToken());
+        M = Integer.parseInt(tokenizer.nextToken());
+        K = Integer.parseInt(tokenizer.nextToken());
+
+        int[][] map = new int[N + 1][M + 1];
+        for (int i = 1; i <= N; i++) {
+            String line = br.readLine();
+            for (int j = 1; j <= M; j++) {
+                map[i][j] = line.charAt(j - 1) - '0';
+            }
+        }
+        sb.append(bfs(map));
+        
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    private static int bfs(int[][] map) {
+        Queue<Point> queue = new ArrayDeque<>();
+        queue.add(new Point(1, 1, K, 0));
+        boolean[][][] visited = new boolean[N + 1][M + 1][K + 1];
+        visited[1][1][K] = true;
+        while (!queue.isEmpty()) {
+            Point cur = queue.poll();
+            int x = cur.x;
+            int y = cur.y;
+            int k = cur.k;
+            int d = cur.d;
+
+            if (x == N && y == M) {
+                return d + 1;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (!isInRange(nx, ny)) {
+                    continue;
+                }
+                
+                if (k > 0) {
+                    if (map[nx][ny] == 1 && !visited[nx][ny][k - 1]) {
+                        visited[nx][ny][k - 1] = true;
+                        queue.add(new Point(nx, ny, k - 1, d + 1));
+                    }
+                }
+
+                if (!visited[nx][ny][k] && map[nx][ny] == 0) {
+                    visited[nx][ny][k] = true;
+                    queue.add(new Point(nx, ny, k, d + 1));
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static boolean isInRange(int x, int y) {
+        return x >= 1 && y >= 1 && x <= N && y <= M;
+    }
+
+    static class Point {
+        int x, y, k, d;
+
+        public Point(int x, int y, int k, int d) {
+            this.x = x;
+            this.y = y;
+            this.k = k;
+            this.d = d;
+        }
+    }
+}
