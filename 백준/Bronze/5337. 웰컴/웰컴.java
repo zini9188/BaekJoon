910e79f062ab3126a43528b3bb464ebc5d624384
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-09-08 23:21:35 +0900

    [Bronze V] Title: 웰컴, Time: 112 ms, Memory: 13824 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        sb.append(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+        System.out.println(sb.toString());
+    }
+}
