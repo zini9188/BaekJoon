e2675e32ad2c0ddb194a7e7ee250aed7ad13bfab
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-09-25 11:40:35 +0900

    [Gold IV] Title: 연구소, Time: 1152 ms, Memory: 298960 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -13,7 +12,6 @@ public class Main {
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
     static int answer = 0;
-    static ArrayList<Point> virus = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         StringTokenizer tokenizer = new StringTokenizer(br.readLine());
@@ -25,13 +23,10 @@ public class Main {
             tokenizer = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 lab[i][j] = Integer.parseInt(tokenizer.nextToken());
-                if (lab[i][j] == 2) {
-                    virus.add(new Point(i, j));
-                }
             }
         }
 
-        buildWall(0, 0);
+        buildWall(0, 0, 0);
 
         bw.write(answer + "");
         bw.flush();
@@ -39,17 +34,17 @@ public class Main {
         br.close();
     }
 
-    private static void buildWall(int nx, int count) {
+    private static void buildWall(int nx, int ny, int count) {
         if (count == 3) {
             answer = Math.max(answer, spread());
             return;
         }
 
         for (int i = nx; i < N; i++) {
-            for (int j = 0; j < M; j++) {
+            for (int j = ny; j < M; j++) {
                 if (lab[i][j] == 0) {
                     lab[i][j] = 1;
-                    buildWall(nx, count + 1);
+                    buildWall(nx, ny, count + 1);
                     lab[i][j] = 0;
                 }
             }
@@ -59,22 +54,24 @@ public class Main {
     private static int spread() {
         int[][] copy = new int[N][M];
         for (int i = 0; i < N; i++) {
-            if (M >= 0) System.arraycopy(lab[i], 0, copy[i], 0, M);
+            copy[i] = lab[i].clone();
         }
 
-        for (Point point : virus) {
-            if (copy[point.x][point.y] == 2) {
-                Queue<Point> q = new LinkedList<>();
-                q.add(new Point(point.x, point.y));
-                while (!q.isEmpty()) {
-                    Point cur = q.poll();
-                    for (int dir = 0; dir < 4; dir++) {
-                        int nx = cur.x + dx[dir];
-                        int ny = cur.y + dy[dir];
+        Queue<Point> virus = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (copy[i][j] == 2) {
+                    virus.add(new Point(i, j));
+                    while (!virus.isEmpty()) {
+                        Point cur = virus.poll();
+                        for (int dir = 0; dir < 4; dir++) {
+                            int nx = cur.x + dx[dir];
+                            int ny = cur.y + dy[dir];
 
-                        if (isInRange(nx, ny) && copy[nx][ny] == 0) {
-                            q.add(new Point(nx, ny));
-                            copy[nx][ny] = 2;
+                            if (isInRange(nx, ny) && copy[nx][ny] == 0) {
+                                virus.add(new Point(nx, ny));
+                                copy[nx][ny] = 2;
+                            }
                         }
                     }
                 }
