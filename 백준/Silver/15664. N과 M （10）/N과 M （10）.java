70bb50a7e7ce7b14b710b4c414015cc6f9cc7f72
Ho Je <83208807+zini9188@users.noreply.github.com>
2023-12-20 17:49:14 +0900

    [Silver II] Title: N과 M (10), Time: 128 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int[] arr;
+    static Set<String> set;
+    static int N, M;
+    static int[] temp;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        set = new LinkedHashSet<>();
+        arr = new int[N];
+        temp = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        find(0, 0);
+        for (String s : set) {
+            sb.append(s).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    private static void find(int depth, int index) {
+        if (depth == M) {
+            StringBuilder b = new StringBuilder();
+            for (int i = 0; i < M; i++) {
+                b.append(temp[i]).append(" ");
+            }
+            set.add(b.toString());
+            return;
+        }
+
+        for (int i = index; i < N; i++) {
+            temp[depth] = arr[i];
+            find(depth + 1, i + 1);
+            temp[depth] = 0;
+        }
+    }
+}
