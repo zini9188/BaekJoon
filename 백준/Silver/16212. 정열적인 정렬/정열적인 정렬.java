cb5d09efb5e2a21182cb7b127e8d5c29a484fe33
Ho Je <83208807+zini9188@users.noreply.github.com>
2024-04-12 08:28:05 +0900

    [Silver V] Title: 정열적인 정렬, Time: 1520 ms, Memory: 81924 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for (int i : arr) {
+            sb.append(i).append(" ");
+        }
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+}
