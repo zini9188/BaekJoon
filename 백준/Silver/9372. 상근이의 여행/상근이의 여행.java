36d3040d4d7bd2ec13d1521a293ccf501a613455
Ho Je <83208807+zini9188@users.noreply.github.com>
2024-06-16 12:15:38 +0900

    [Silver IV] Title: 상근이의 여행, Time: 232 ms, Memory: 24532 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            for (int i = 0; i < M; i++) {
+                br.readLine();
+            }
+            sb.append(N - 1).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+}
