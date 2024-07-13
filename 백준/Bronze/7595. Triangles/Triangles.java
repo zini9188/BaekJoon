965f2d443a181d882293e4b99e6740fe0e8b0f16
Ho Je <83208807+zini9188@users.noreply.github.com>
2024-07-13 15:43:01 +0900

    [Bronze IV] Title: Triangles, Time: 132 ms, Memory: 17400 KB -BaekjoonHub

---
+++ 백준/Bronze/7595. Triangles/Triangles.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder sb = new StringBuilder();
+    private static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            int n = read();
+            if (n == 0) {
+                break;
+            }
+            for (int j = 0; j < n; j++) {
+                sb.append("*".repeat(j + 1));
+                sb.append("\n");
+            }
+        }
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
