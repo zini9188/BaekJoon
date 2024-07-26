3279230d5bea1f0fc84c233935d946112eeb46e7
Ho Je <83208807+zini9188@users.noreply.github.com>
2024-07-27 01:19:11 +0900

    [Platinum IV] Title: 가톨릭대학교에 워터 슬라이드를??, Time: 324 ms, Memory: 33544 KB -BaekjoonHub

---
+++ 백준/Platinum/18133. 가톨릭대학교에 워터 슬라이드를？？/가톨릭대학교에 워터 슬라이드를？？.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder sb = new StringBuilder();
+    private static List<List<Integer>> graph;
+    private static Stack<Integer> stack;
+    private static int N, M;
+    private static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solution();
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    private static void solution() {
+        visited = new boolean[N + 1];
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                dfs(i);
+            }
+        }
+
+        visited = new boolean[N + 1];
+
+        int cnt = 0;
+        while (!stack.isEmpty()) {
+            Integer v = stack.pop();
+
+            if (!visited[v]) {
+                cnt++;
+                dfs(v);
+            }
+        }
+
+        sb.append(cnt);
+    }
+
+    private static void dfs(int x) {
+        visited[x] = true;
+
+        for (Integer next : graph.get(x)) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+
+        stack.add(x);
+    }
+
+    private static void input() throws IOException {
+        N = read();
+        M = read();
+
+        graph = new ArrayList<>();
+        stack = new Stack<>();
+
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            int v = read();
+            int w = read();
+
+            graph.get(v).add(w);
+        }
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
