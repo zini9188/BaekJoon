2f81895d7ee19e583483a08ef0dd3ea7a043691b
Ho Je <83208807+zini9188@users.noreply.github.com>
2024-07-27 03:30:54 +0900

    [Platinum IV] Title: 축구 전술, Time: 1104 ms, Memory: 238708 KB -BaekjoonHub

---
+++ 백준/Platinum/3977. 축구 전술/축구 전술.java
@@ -0,0 +1,119 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder sb = new StringBuilder();
+    private static StringTokenizer st;
+    private static List<List<Integer>> graph, reverse;
+    private static List<Integer> ans;
+    private static Stack<Integer> stack;
+    private static int N, M;
+    private static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            input();
+            solution();
+            br.readLine();
+            sb.append("\n");
+        }
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    private static void solution() {
+        visited = new boolean[N + 1];
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                dfs(i);
+            }
+        }
+
+        ans = new ArrayList<>();
+        visited = new boolean[N + 1];
+        int v = stack.pop();
+        int cnt = check(v);
+
+        if (cnt == N) {
+            visited = new boolean[N + 1];
+            kosaraju(v);
+            Collections.sort(ans);
+            for (Integer n : ans) {
+                sb.append(n).append("\n");
+            }
+        } else {
+            sb.append("Confused\n");
+        }
+    }
+
+    private static int check(int x) {
+        int cnt = 1;
+        visited[x] = true;
+        for (Integer next : graph.get(x)) {
+            if (!visited[next]) {
+                cnt += check(next);
+            }
+        }
+
+        return cnt;
+    }
+
+    private static void kosaraju(int x) {
+        visited[x] = true;
+        for (Integer next : reverse.get(x)) {
+            if (!visited[next]) {
+                kosaraju(next);
+            }
+        }
+
+        ans.add(x);
+    }
+
+    private static void dfs(int x) {
+        visited[x] = true;
+        for (Integer next : graph.get(x)) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+
+        stack.add(x);
+    }
+
+    private static void input() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList<>();
+        reverse = new ArrayList<>();
+        stack = new Stack<>();
+
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+            reverse.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph.get(a).add(b);
+            reverse.get(b).add(a);
+        }
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
