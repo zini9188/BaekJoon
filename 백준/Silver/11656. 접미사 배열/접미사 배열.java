37c207aa276a6cd81cb07c8d46a720122cd368eb
Ho Je <83208807+zini9188@users.noreply.github.com>
2024-08-02 15:00:13 +0900

    [Silver IV] Title: 접미사 배열, Time: 172 ms, Memory: 18072 KB -BaekjoonHub

---
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder sb = new StringBuilder();
+    private static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        String input = br.readLine();
+        int len = input.length();
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < len; i++) {
+            set.add(input.substring(i));
+        }
+
+        set.stream().sorted().forEach(o -> sb.append(o).append("\n"));
+
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
