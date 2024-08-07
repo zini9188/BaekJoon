13481c3d15109df9dc31d4bb5e6569c89e036f52
Ho Je <83208807+zini9188@users.noreply.github.com>
2024-08-07 10:58:42 +0900

    [Silver II] Title: 편의점 2, Time: 412 ms, Memory: 25820 KB -BaekjoonHub

--- 백준/Silver/14400. 편의점 2/편의점 2.java
+++ 백준/Silver/14400. 편의점 2/편의점 2.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
 
@@ -9,28 +8,40 @@ public class Main {
     private static final StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
-        int N = Integer.parseInt(br.readLine().trim());
+        int N = read();
         int[] x = new int[N], y = new int[N];
-        StringTokenizer st;
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            x[i] = Integer.parseInt(st.nextToken());
-            y[i] = Integer.parseInt(st.nextToken());
+            x[i] = read();
+            y[i] = read();
         }
         Arrays.sort(x);
         Arrays.sort(y);
-        int idx = N / 2;
-        if (N % 2 == 0) {
-            idx--;
-        }
-
         long ans = 0;
         for (int i = 0; i < N; i++) {
-            ans += Math.abs(x[i] - x[idx]) + Math.abs(y[i] - y[idx]);
+            ans += Math.abs(x[i] - x[N / 2]) + Math.abs(y[i] - y[N / 2]);
         }
         sb.append(ans);
         bw.write(sb.toString());
         bw.close();
         br.close();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read();
+        while (n <= ' ') {
+            n = System.in.read();
+        }
+        n &= 15;
+
+        boolean negative = n == 13;
+        if (negative) {
+            n = 0;
+        }
+
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+
+        return negative ? -n : n;
     }
 }
