074068b2e91b38481d913a4c20a8e38eaf54bfe4
Ho Je <83208807+zini9188@users.noreply.github.com>
2024-08-07 10:33:35 +0900

    [Bronze III] Title: 통학의 신, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/17945. 통학의 신/통학의 신.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int a = read();
+        int b = read();
+        for (int i = -1000; i <= 1000; i++) {
+            if (i * i + (a * i * 2) + b == 0) {
+                sb.append(i).append(" ");
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        boolean negative = n == 13;
+        if (negative) {
+            n = 0;
+        }
+
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+
+        if (c == 13) {
+            System.in.read();
+        }
+
+        return negative ? -n : n;
+    }
+}
