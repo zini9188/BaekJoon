80ffbad8975776a56376a91a6d8f784e5db884a7
Ho Je <83208807+zini9188@users.noreply.github.com>
2024-08-14 15:45:48 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int N = read();
+        int M = read();
+        int K = read();
+
+        int cnt = 0;
+        int boy = 0, girl = 0;
+        while ((boy < M && girl < N - 1) && boy + girl + K <= N + M - 3) {
+            boy++;
+            girl += 2;
+            cnt++;
+        }
+
+        sb.append(cnt);
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
