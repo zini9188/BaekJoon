3c009c4ab4526fc61d5140e5821e3e9049dea394
Ho Je <83208807+zini9188@users.noreply.github.com>
2024-08-16 13:56:30 +0900

    [Bronze III] Title: 완전 제곱수, Time: 116 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Bronze/6131. 완전 제곱수/완전 제곱수.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int N = read();
+
+        int ans = 0;
+        for (int i = 1; i <= 500; i++) {
+            for (int j = 1; j <= 500; j++) {
+                if (Math.pow(j, 2) + N == Math.pow(i, 2)) {
+                    ans++;
+                }
+            }
+        }
+
+        sb.append(ans);
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
