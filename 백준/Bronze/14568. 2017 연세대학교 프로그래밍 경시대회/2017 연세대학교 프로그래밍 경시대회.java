fa7b70eff31577bccbb9ef3e8e16b0d0641c3840
Ho Je <83208807+zini9188@users.noreply.github.com>
2024-08-16 14:00:02 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 100 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int N = read();
+
+        int ans = 0;
+        for (int i = 2; i <= N - 2; i += 2) {
+            for (int j = 1; j <= N - 1; j++) {
+                int k = N - j - i;
+                if (k >= j + 2) {
+                    ans++;
+                }
+            }
+        }
+
+        sb.append(ans);
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
