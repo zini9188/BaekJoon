f521d8d23896e96db579b06f1c791f583a2a4e42
Ho Je <83208807+zini9188@users.noreply.github.com>
2024-08-16 14:04:15 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 100 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int sum = 0;
+        int ans = 0;
+        int diff = Integer.MAX_VALUE;
+        for (int i = 0; i < 10; i++) {
+            sum += read();
+
+            int abs = Math.abs(100 - sum);
+            if (diff >= abs) {
+                diff = abs;
+                ans = sum;
+            }
+        }
+
+        sb.append(ans);
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
