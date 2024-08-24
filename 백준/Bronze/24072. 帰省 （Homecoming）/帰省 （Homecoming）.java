77c9af4f2678ec3ef1796112b2d1913545848514
Ho Je <83208807+zini9188@users.noreply.github.com>
2024-08-24 12:11:03 +0900

    [Bronze IV] Title: 帰省 (Homecoming), Time: 100 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/24072. 帰省 （Homecoming）/帰省 （Homecoming）.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int A = read();
+        int B = read();
+        int C = read();
+
+        if (A <= C && C < B) {
+            sb.append("1");
+        } else {
+            sb.append("0");
+        }
+
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        boolean negative = n == 13;
+        if (negative) {
+            n = 0;
+        }
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return negative ? -n : n;
+    }
+}
