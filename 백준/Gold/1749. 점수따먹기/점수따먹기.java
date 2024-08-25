852ee680aa1d15596deb8dfa8e22373a12aa9354
Ho Je <83208807+zini9188@users.noreply.github.com>
2024-08-25 18:55:08 +0900

    [Gold IV] Title: 점수따먹기, Time: 1280 ms, Memory: 15116 KB -BaekjoonHub

---
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.java
@@ -0,0 +1,52 @@
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int n = read();
+        int m = read();
+        int[][] arr = new int[n + 1][m + 1];
+        int[][] prefixSum = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                arr[i][j] = read();
+                prefixSum[i][j] = arr[i][j] + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1];
+            }
+        }
+
+        int ans = Integer.MIN_VALUE;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                for (int k = i; k <= n; k++) {
+                    for (int l = j; l <= m; l++) {
+                        int temp = prefixSum[k][l] - prefixSum[k][j - 1] - prefixSum[i - 1][l] + prefixSum[i - 1][j - 1];
+                        if (temp > ans) {
+                            ans = temp;
+                        }
+                    }
+                }
+            }
+        }
+
+        sb.append(ans);
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        boolean negative = n == 13;
+        if (negative) {
+            n = 0;
+        }
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return negative ? -n : n;
+    }
+}
