ca8476e41cea9423e79d29cee3091878e4793d35
Ho Je <83208807+zini9188@users.noreply.github.com>
2024-08-26 20:37:50 +0900

    [Silver III] Title: 합 구하기, Time: 232 ms, Memory: 21204 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int N = read();
+
+        int[] arr = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            arr[i] = read() + arr[i - 1];
+        }
+
+        int M = read();
+        for (int i = 0; i < M; i++) {
+            int a = read();
+            int b = read();
+            sb.append(arr[b] - arr[a - 1]).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        boolean negative = n == 13;
+        if (negative) {
+            n = 0;
+        }
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return negative ? -n : n;
+    }
+}
