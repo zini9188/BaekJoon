e3d93c987207c05a65abac30685d335b63171b26
Ho Je <83208807+zini9188@users.noreply.github.com>
2024-08-29 20:04:44 +0900

    [Silver IV] Title: 주몽, Time: 124 ms, Memory: 15888 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int N = read();
+        int M = read();
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = read();
+        }
+        Arrays.sort(arr);
+
+        int ans = 0;
+        for (int left = 0, right = N - 1; left < right; ) {
+            int sum = arr[left] + arr[right];
+            if (sum > M) {
+                right--;
+            } else if (sum == M) {
+                ans++;
+                left++;
+                right--;
+            } else {
+                left++;
+            }
+        }
+
+        sb.append(ans);
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        boolean negative = n == 13;
+        if (negative) {
+            n = 0;
+        }
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return negative ? -n : n;
+    }
+}
