7ff9f544e23d757b09abe93b8ddf84fe07d5d031
Ho Je <83208807+zini9188@users.noreply.github.com>
2024-09-04 21:16:21 +0900

    [Bronze IV] Title: Fill the Rowboats!, Time: 96 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze/5300. Fill the Rowboats！/Fill the Rowboats！.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int N = read();
+
+        for (int i = 1; i <= N; i++) {
+            sb.append(i);
+            if (i == N) {
+                sb.append(" Go!");
+                break;
+            }
+
+            if (i % 6 == 0) {
+                sb.append(" Go! ");
+            } else {
+                sb.append(" ");
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        boolean negative = n == 13;
+        if (negative) {
+            n = 0;
+        }
+
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+
+        return negative ? -n : n;
+    }
+}
