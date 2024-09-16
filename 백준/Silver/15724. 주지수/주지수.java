2f9d1ed60e326fd2cc591387330a52994cc7b86b
Ho Je <83208807+zini9188@users.noreply.github.com>
2024-09-16 13:50:08 +0900

    [Silver I] Title: 주지수, Time: 304 ms, Memory: 23948 KB -BaekjoonHub

---
+++ 백준/Silver/15724. 주지수/주지수.java
@@ -0,0 +1,49 @@
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int N = read();
+        int M = read();
+
+        int[][] arr = new int[N + 1][M + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                arr[i][j] = arr[i - 1][j] + arr[i][j - 1] + read() - arr[i - 1][j - 1];
+            }
+        }
+
+        int K = read();
+        for (int i = 0; i < K; i++) {
+            int x1 = read();
+            int y1 = read();
+            int x2 = read();
+            int y2 = read();
+
+            sb.append(arr[x2][y2] - arr[x2][y1 - 1] - arr[x1 - 1][y2] + arr[x1 - 1][y1 - 1])
+                    .append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        boolean negative = n == 13;
+        if (negative) {
+            n = 0;
+        }
+
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+
+        return negative ? -n : n;
+    }
+}
