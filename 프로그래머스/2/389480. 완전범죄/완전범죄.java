3cf56175ff0e969168f07f6dbde8ea6e546d2f3d
Ho Je <83208807+zini9188@users.noreply.github.com>
2025-09-17 01:18:51 +0900

    [level 2] Title: 완전범죄, Time: 21.73 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Solution {
+    
+    static int N, M;
+    static int[][] INFO;
+    static int ANS = Integer.MAX_VALUE;
+    static Set<String> set;
+    
+    public int solution(int[][] info, int n, int m) {
+        int answer = 0;
+        INFO = info;
+        N = n;
+        M = m;
+        set = new HashSet<>(); 
+        
+        dfs(0, 0, 0);
+        
+        return ANS == Integer.MAX_VALUE ? -1 : ANS;
+    }
+    
+    public static void dfs(int idx, int n, int m) {
+        if(idx > INFO.length || n >= ANS) {
+            return;
+        }
+        
+        if(idx == INFO.length) {
+            ANS = Math.min(ANS, n);
+            return;
+        }
+        
+        String data = idx + ":" + n + ":" + m;
+        if(set.contains(data)) {
+            return;
+        }
+        
+        if(INFO[idx][0] + n < N){
+            dfs(idx + 1, n + INFO[idx][0], m);                   
+        }
+        if(INFO[idx][1] + m < M) {
+            dfs(idx + 1, n, m + INFO[idx][1]);
+        }
+        set.add(data);
+    }
+}
